6391be12696d9795b279f22337878653f046b409
Karyn <k251257@dac.unicamp.br>
2025-11-21 22:02:40 -0300

    Lista 3 e ajeitando comentarios

---
+++ Lista_3/PedraFilosofal.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+   
+    return 0;
+}
